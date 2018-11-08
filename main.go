cd37b4375e6cc233d8265def0286bf1a6230a3c5
Giovanni Spera <giovanni.spera2011@libero.it>
2018-11-09 00:33:28 +0100

    Moved the indexing to a custom function

--- main.go
+++ main.go
@@ -21,30 +21,47 @@ func main() {
 		os.Mkdir(functionsDir, 0766)
 	}
 
-	fns, err := ioutil.ReadDir(functionsDir)
+	fns := index(functionsDir)
+
+	for _, fn := range fns {
+		log("Handling %s\n", fn.route)
+		http.HandleFunc(fn.route, fn.handler)
+	}
+
+	log("Listening: %s\n", addr)
+	http.ListenAndServe(addr, nil)
+}
+
+//index indexes the folder given as parameter and returns all the functions contained inside the folder
+func index(folder string) []function {
+	var functions []function
+
+	log("Indexing: %s\n", folder)
+
+	fns, err := ioutil.ReadDir(folder)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Cannot read %s directory: %v\n", functionsDir, err)
 		os.Exit(1)
 	}
 
+	functions = make([]function, 0, len(fns))
+
 	for _, fn := range fns {
 		if fn.IsDir() { //Sub-directory are not supported yet
-			log("Skipping directory: %s\n", fn.Name())
+			log(" -Skipping directory: %s\n", fn.Name())
 			continue
 		}
 
 		log("Loading plugin: %s\n", fn.Name())
-		plPath := path.Join(functionsDir, fn.Name())
-		log(" -Path: %s\n", plPath)
+		plPath := path.Join(folder, fn.Name())
+		log("  -Path: %s\n", plPath)
 		pl, err := loadFunction(plPath)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot load plugin %s: %v\n", fn.Name(), err)
 		}
 
-		log(" -Handling %s: %s\n", pl.route, fn.Name())
-		http.HandleFunc(pl.route, pl.handler)
+		functions = append(functions, pl)
 	}
 
-	log("Listening: %s\n", addr)
-	http.ListenAndServe(addr, nil)
+	return functions
 }
